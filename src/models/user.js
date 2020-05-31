3ac503ab5648feeb0250775b6eaf900423a7bcb9
Can Ural <can.ural@outlook.com>
2020-05-31 15:53:14 +0300

    Created user schema. Added mongoose middlewares to hash the password before its saved to database, check if the hashed password is same with the user password(when signing in) and adding tokens to user in db. Added jwt tokens to user schema, saving them each time user logs in or registers. Refactored user router for patch so mongoose middleware can be used (findByIdAndUpdate bypasses middlewares)

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -29,6 +31,51 @@ const User = mongoose.model('User', {
       }
     },
   },
+  tokens: [
+    {
+      token: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
 });
+
+userSchema.methods.generateAuthToken = async function () {
+  const user = this;
+  const token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET);
+
+  user.tokens = user.tokens.concat({ token });
+  await user.save();
+
+  return token;
+};
+
+userSchema.statics.findByCredentials = async (email, password) => {
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw new Error('Unable to login');
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error('Unable to login');
+  }
+
+  return user;
+};
+
+userSchema.pre('save', async function (next) {
+  const user = this;
+
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8);
+  }
+
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,12 +2,26 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 
-router.post('/profile', async (req, res) => {
+router.post('/profile/register', async (req, res) => {
   const user = new User(req.body);
 
   try {
+    const token = await user.generateAuthToken();
     await user.save();
-    res.status(201).send(user);
+    res.status(201).send({ user, token });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+router.post('/profile/login', async (req, res) => {
+  try {
+    const user = await User.findByCredentials(
+      req.body.email,
+      req.body.password
+    );
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -47,10 +61,10 @@ router.patch('/profile/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
+    const user = await User.findById(req.params.id);
+
+    updates.forEach((update) => (user[update] = req.body[update]));
+    await user.save();
 
     if (!user) {
       return res.status(404).send();
