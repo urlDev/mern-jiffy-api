d2a6c738f2e7c2f956e17dcb9f4409c6af0ef98d
Can Ural <can.ural@outlook.com>
2020-06-04 12:53:48 +0300

    Installed cors and added it as a middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require('express');
 require('./db/db');
 const userRouter = require('./routers/user');
 const favoriteRouter = require('./routers/favorite');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(userRouter);
 app.use(favoriteRouter);
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const app = require('./app');
-const port = process.env.PORT;
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
