a07e0e8688d1b377fdc532f1b049dca5af027b83
Can Ural <can.ural@outlook.com>
2020-06-07 18:52:01 +0300

    Added adorable avatars library. Created an express middleware to have an endpoint for avatars

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ require('./db/db');
 const userRouter = require('./routers/user');
 const favoriteRouter = require('./routers/favorite');
 const cors = require('cors');
+const avatarsMiddleware = require('adorable-avatars');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/myAvatars', avatarsMiddleware);
 app.use(userRouter);
 app.use(favoriteRouter);
 
